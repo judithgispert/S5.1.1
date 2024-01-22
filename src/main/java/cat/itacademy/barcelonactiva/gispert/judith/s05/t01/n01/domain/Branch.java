187d58704b05e890dc8aa26450365313e3b8de8c
judithgispert <jugispert@gmail.com>
2024-01-22 13:20:18 +0100

    Entity branch + methods

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/domain/Branch.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/domain/Branch.java
@@ -1,4 +1,35 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n01.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "branch")
 public class Branch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pk_BranchId;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+    @Column(name = "country", nullable = false, length = 50)
+    private String country;
+
+    public Branch(String name, String country){
+        this.name = name;
+        this.country = country;
+    }
+
+    public int getPk_BranchId(){return pk_BranchId;}
+    public String getName(){return name;}
+    public String getCountry(){return country;}
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCountry(String country){
+        this.country = country;
+    }
+
+    @Override
+    public String toString(){return "Id: " + pk_BranchId
+            + ", Name: " + name + " - Country: " + country;}
 }
--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/dto/BranchDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/dto/BranchDTO.java
@@ -1,4 +1,12 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n01.dto;
 
-public class BranchDTO {
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n01.domain.Branch;
+import jakarta.persistence.Column;
+
+public class BranchDTO extends Branch {
+    @Column(name = "name", nullable = false, length = 50)
+    private String type;
+    public BranchDTO(String name, String country) {
+        super(name, country);
+    }
 }
