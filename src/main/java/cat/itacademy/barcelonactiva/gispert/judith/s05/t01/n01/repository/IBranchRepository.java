14e04a08de01e7c6606670a12730a32759574481
judithgispert <jugispert@gmail.com>
2024-01-22 17:12:25 +0100

    Repo extends Jpa

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/repository/IBranchRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/repository/IBranchRepository.java
@@ -1,4 +1,9 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n01.repository;
 
-public interface IBranchRepository {
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n01.domain.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBranchRepository extends JpaRepository<Branch, Integer> {
 }
