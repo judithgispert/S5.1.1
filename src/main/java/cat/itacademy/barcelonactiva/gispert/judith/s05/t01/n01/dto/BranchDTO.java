1e721a6edb1c61628df727a3c9f774c1e726e768
judithgispert <jugispert@gmail.com>
2024-01-24 10:25:57 +0100

    Corrections dto

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/dto/BranchDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n01/dto/BranchDTO.java
@@ -7,26 +7,39 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class BranchDTO extends Branch {
+public class BranchDTO {
     @Column(name = "type", nullable = false, length = 50)
     private String type;
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+    @Column(name = "country", nullable = false, length = 50)
+    private String country;
     private final static List<String> countriesUE = new ArrayList<>(Arrays.asList("Austria", "Belgium", "Bulgaria", "Croatia", "Republic of Cyprus",
             "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy",
             "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia",
             "Slovenia", "Spain", "Sweden"));
     public BranchDTO(String name, String country) {
-        super(name, country);
-        this.type = searchType();
+        this.name = name;
+        this.country = country;
+        type = searchType();
     }
 
     public String getType(){return type;}
+    public String getName(){return name;}
+    public String getCountry(){return country;}
     public List<String> getCountriesUE(){return countriesUE;}
     public void setType(String type){
         this.type = type;
     }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCountry(String country){
+        this.country = country;
+    }
 
     public String searchType(){
-        boolean exist = countriesUE.contains(getCountry());
+        boolean exist = countriesUE.contains(country);
         if (!exist){
             type = "Outside UE";
         } else {
